24db1650f87635090d653dc13ab6a9fdb17e472b
Luke Gilbert <lukegilbert.416@gmail.com>
2024-11-19 21:56:12 +0000

    finished rps ui

--- rps.js
+++ rps.js
@@ -1,32 +1,18 @@
-function getPlayerChoice() {
-  let validChoice = false;
-  let playerChoice = "";
-  while (!validChoice) {
-    playerChoice = prompt("Please enter either Rock, Paper or Scissors: ");
-    if (
-      playerChoice.toLowerCase() == "rock" ||
-      playerChoice.toLowerCase() == "paper" ||
-      playerChoice.toLowerCase() == "scissors"
-    ) {
-      validChoice = true;
-    } else {
-      console.log("Enter a valid choice");
-    }
-  }
-  return playerChoice.toLowerCase();
+function setPlayerChoice(choice) {
+  return choice;
 }
 
 // Randomly choose rock, paper or scissors for the CPU
-function getCPUChoice() {
+function setCPUChoice() {
   let choices = ["rock", "paper", "scissors"];
   let randomChoice = Math.floor(Math.random() * 3);
-  console.log(randomChoice);
   return choices[randomChoice];
 }
 
-function decideBout(playerChoice, cpuChoice) {
+function decideBout(playerChoice) {
   // Let result be an integer 0,1 or 2. 0 being tie, 1 being player win and 2 being cpu win
   let result;
+  let cpuChoice = setCPUChoice();
   // Determine the winner of a bout with the following rules: Rock beats scissors, Scissors beats paper, Paper beats rock and same ties.
   if (playerChoice == "rock" && cpuChoice == "scissors") {
     result = 1;
@@ -39,39 +25,28 @@ function decideBout(playerChoice, cpuChoice) {
   } else {
     result = 2;
   }
-
+  incrementScores(result);
   return result;
 }
 
-// Initialise both players scores to 0
-let playerScore = 0;
-let cpuScore = 0;
-let playerChoice;
-let cpuChoice;
-// End game when one player score reaches 3
-// Add 1 to the winners score and repeat steps 2 to 4
-while (playerScore < 3 && cpuScore < 3) {
-  // Take users input for their choice of rock, paper or scissors
-  playerChoice = getPlayerChoice();
-  cpuChoice = getCPUChoice();
-  console.log(`The Player has chosen: ${playerChoice}`);
-  console.log(`The CPU has chosen: ${cpuChoice}`);
-  console.log(`Fight!`);
-  if (decideBout(playerChoice, cpuChoice) == 1) {
+function incrementScores(result) {
+  if (result == 1) {
     playerScore += 1;
-    console.log("Player Wins!");
-  } else if (decideBout(playerChoice, cpuChoice) == 2) {
+    roundCounter += 1;
+  } else if (result == 2) {
     cpuScore += 1;
-    console.log("CPU Wins!");
-  } else {
-    console.log("It's a tie! Play Again!");
+    roundCounter += 1;
   }
-  console.log(`Player Score: ${playerScore}`);
-  console.log(`Computer Score: ${cpuScore}`);
+  document.getElementById("round-no").textContent = roundCounter;
+  document.getElementById("player-score").textContent = playerScore;
+  document.getElementById("cpu-score").textContent = cpuScore;
 }
 
-if (playerScore >= cpuScore) {
-  console.log("Humans reign supreme or machine!");
-} else {
-  console.log("Quick! Run! The machines have won!");
-}
+// Initialise the game
+let playerScore = 0;
+let cpuScore = 0;
+let roundCounter = 1;
+
+// End game when one player score reaches 3
+// Add 1 to the winners score and repeat steps 2 to 4
+// Take users input for their choice of rock, paper or scissors
